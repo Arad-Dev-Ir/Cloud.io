c5ccd8aa8188de0cf856e916d65ff1adb4a739c4
Farshid Kazemi <farshidkazemi.dev@outlook.com>
2024-06-01 09:42:26 +0330

    feature: Add domain of News
    This commit adds the News feature domain to the application, which allows to registration of News.
    
    The feature includes creating a News.

---
+++ Solutions/News/src/Core/NewsManagement.Core.Domain/Application/News/Models/Entity/Keyword.cs
@@ -0,0 +1,22 @@
+﻿namespace NewsManagement.Core.News.Models;
+
+using Cloud.Core.Models;
+
+public class Keyword : Entity
+{
+    public Code KeywordCode { get; private set; }
+
+    public Keyword(Code code)
+    => Initialize(code);
+
+    private void Initialize(Code code, Action? act = default)
+    {
+        act?.Invoke();
+        KeywordCode = code;
+    }
+
+    public static Keyword Instance(Code code)
+    => new(code);
+    public static Keyword Instance(string code)
+    => new(code);
+}
